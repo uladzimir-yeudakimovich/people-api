562fe776ac0fa31294be91098b6956ff168a283d
Uladzimir <uladzimir.yeudakimovich@gmail.com>
2018-06-13 01:15:58 +0300

    update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { AppComponent } from './app.component';
-
 import { environment } from '../environments/environment';
 
 @NgModule({
@@ -18,4 +15,5 @@ import { environment } from '../environments/environment';
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
